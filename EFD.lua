d8698ebf28e9091a041a1dbb0d33cb2a2eedea4d
Terisus <92530212+Terisus@users.noreply.github.com>
2022-05-30 16:13:57 +0800

    Update EFD.lua

--- EFD.lua
+++ EFD.lua
@@ -13,36 +13,3 @@ _G.UseTeamColor = false
 b:Button("Delete Traps",function()
 	game:GetService("Workspace").KillerItems["BearTrap"]:Destroy()
     end)
-
-if _G.Reantheajfdfjdgs then
-    return
-end
-
-_G.Reantheajfdfjdgs = ":suifayhgvsdghfsfkajewfrhk321rk213kjrgkhj432rj34f67df"
-
-local players = game:GetService("Players")
-local plr = players.LocalPlayer
-
-function esp(target, color)
-    if target.Character then
-        if not target.Character:FindFirstChild("GetReal") then
-            local highlight = Instance.new("Highlight")
-            highlight.RobloxLocked = true
-            highlight.Name = "GetReal"
-            highlight.Adornee = target.Character
-            highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            highlight.FillColor = color
-            highlight.Parent = target.Character
-        else
-            target.Character.GetReal.FillColor = color
-        end
-    end
-end
-
-while task.wait() do
-    for i, v in pairs(players:GetPlayers()) do
-        if v ~= plr then
-            esp(v, _G.UseTeamColor and v.TeamColor.Color or ((plr.TeamColor == v.TeamColor) and _G.FriendColor or _G.EnemyColor))
-        end
-    end
-end
